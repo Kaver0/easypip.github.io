556bfb75ac820d86749fa6b8c0436c6ccf594d7b
Kaver0 <101170831+Kaver0@users.noreply.github.com>
2022-03-08 13:45:55 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,18 @@
-test = 90
+import wget
+
+input_start = "==>>> "
+upgradePIPstring = "upgrade"
+
+def removed():
+	print("--- PIP успешно обновился! ---\nЧтоб применить изменения перезапустите программу!")
+
+def main(user_input):
+	if user_input == upgradePIPstring:
+		return "remove"
+	elif user_input == "install":
+		install_input = input("INSTALL >>> ")
+		try:
+			wget.download("https://raw.githubusercontent.com/Kaver0/easypip.github.io/main/modules/" + install_input + ".py", out="installer.py")
+			print("\n-> Вы скачали инсталятор {0}!\n-> Чтоб скачать необходимые модули перенесите файл 'installer.py' в необходимую папку, а затем откройте этот файл.\n-> После загрузки модулей, файл-инсталятор можно удалить.".format(install_input))
+		except Exception as _ex:
+			print("==================== Ошибка ====================\n-> Неизвестный модуль!\n-> Пожалуйста проверьте название модуля!\n================================================")
